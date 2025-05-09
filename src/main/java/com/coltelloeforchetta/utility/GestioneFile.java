7ce7195a651adfd29e2ca9bf223ee05fd09f6584
BiskoBerty <ricky.galimberti@gmail.com>
2025-05-09 11:59:21 +0200

    Create getAllRows method

--- src/main/java/com/coltelloeforchetta/utility/GestioneFile.java
+++ src/main/java/com/coltelloeforchetta/utility/GestioneFile.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class GestioneFile {
     private String percorsoRelativo;
@@ -44,6 +45,34 @@ public class GestioneFile {
             System.out.println("Errore di lettura: " + e.getMessage());
         }
     }
+
+
+    private String[] getAllRows(){
+        ArrayList<String> righe = new ArrayList<>();
+        Path path = Paths.get(percorsoRelativo);
+        if (!Files.exists(path)) {
+            System.out.println("Errore: Il file '" + percorsoRelativo + "' non esiste!");
+            return null;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(percorsoRelativo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                righe.add(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Errore di lettura: " + e.getMessage());
+        }
+        String[] righeArray = new String[righe.size()];
+        for (int i = 0; i < righe.size(); i++) {
+            righeArray[i] = righe.get(i);
+        }
+        return righeArray;
+    }
+
+    public void cercaMatch(){
+
+    }  
+
 }
 
 

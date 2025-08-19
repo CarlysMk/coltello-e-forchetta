dbeb9a43a88a7b34ae40904c8011fe96e54ff38b
BiskoBerty <ricky.galimberti@gmail.com>
2025-08-19 14:59:16 +0200

    aggionato utente

--- src/main/java/com/coltelloeforchetta/common/utente.java
+++ src/main/java/com/coltelloeforchetta/common/utente.java
@@ -8,6 +8,45 @@
 
 package com.coltelloeforchetta.common;
 
+import com.coltelloeforchetta.utility.GestioneDB;
+import org.mindrot.jbcrypt.BCrypt;
+
 public class utente {
-    // TODO
+
+    private String nomeUtente, cognomeUtente, username, ClearPassword, dataNascita, ruolo;
+    private String [] domicilio = new String[3];
+    private String HashPsw;
+    boolean boolRuolo; // TRUE per ristoratore, FALSE per utente
+
+    //costruttore per la REGISTRAZIONE di un nuovo utente
+    public utente(String nome, String cognome, String username, String psw, String dataNascita, String ruolo) {
+        this.nomeUtente = nome;
+        this.cognomeUtente = cognome;
+        this.username = username;
+        this.ClearPassword = psw;
+        this.dataNascita = dataNascita;
+        this.ruolo = ruolo;
+        if (this.ruolo.equals("ristoratore")) {
+            this.boolRuolo = true;
+        } else if (this.ruolo.equals("utente")) {
+            this.boolRuolo = false;
+        }
+        HashPsw = BCrypt.hashpw(psw, BCrypt.gensalt());
+        //TODO registrare nel DB il nuovo utente
+    }
+
+    //costruttore per LOGIN utente
+    public utente(String username, String psw){
+        this.username = username;
+        this.ClearPassword = psw;
+
+        //TODO controllo/login DB
+//        if (db.login(username, psw)) {
+//            System.out.println("Login effettuato con successo");
+//        } else {
+//            System.out.println("Login fallito");
+//        }
+
+        //TODO ottenere i dati completi dal DB e popolare i dati locali
+    }
 }

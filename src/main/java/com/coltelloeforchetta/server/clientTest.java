c240ecf15214acdd6d8b35189b8d0dbb1b2825fd
Giaki03 <79930388+Giaki03@users.noreply.github.com>
2025-08-23 15:52:47 +0200

    test RMI

---
+++ src/main/java/com/coltelloeforchetta/server/clientTest.java
@@ -0,0 +1,44 @@
+package com.coltelloeforchetta.server;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import com.coltelloeforchetta.common.serverTKRemote;
+
+public class clientTest {
+    static int PORT = 1234;
+
+    public static void main(String[] args) {
+
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.getRegistry("127.0.0.1", PORT);
+        } catch (RemoteException e) {
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+        serverTKRemote stub = null;
+        try {
+            stub = (serverTKRemote) registry.lookup("ServerTK");
+        } catch (RemoteException e) {
+            System.out.println(e);
+            System.exit(-2);
+        } catch (NotBoundException e) {
+            System.out.println(e);
+            System.exit(-3);
+        }
+
+        System.out.println( "Client started" );
+
+
+        try {
+            stub.metodo();
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/coltelloeforchetta/server/serverTK.java
+++ src/main/java/com/coltelloeforchetta/server/serverTK.java
@@ -8,14 +8,69 @@
 
 package com.coltelloeforchetta.server;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import com.coltelloeforchetta.common.serverTKRemote;
 import com.coltelloeforchetta.server.database.DBConnection;
 
-public class serverTK {
+public class serverTK implements serverTKRemote {
 
-    public static void main(String[] args) {
-        DBConnection db = new DBConnection();
+    static int PORT = 1234;
+
+    public serverTK(){
+        // TODO spostare qua la creazione dell'oggetto DBConnection + aggiungere eventuali opzioni di setup
+    }
+
+    public static void main(String[] args)  {
+        //DBConnection db = new DBConnection();
+
+
+        serverTK server = new serverTK();
+        serverTKRemote stub = null;
+        
+        try {
+            stub = (serverTKRemote) UnicastRemoteObject.exportObject(server, PORT);
+        } catch (RemoteException e) {   //eccezione -> porta occupata o porta illegale
+            System.out.println("Porta già occupata");
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+        Registry registry = null;
+        try {
+            registry = LocateRegistry.createRegistry(PORT);
+        } catch (RemoteException e) {
+            System.out.println("Impossibile esportare il registry");
+            System.out.println(e);
+            System.exit(-2);
+        }
+
+        try {
+            registry.bind("ServerTK", stub);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        } catch (AlreadyBoundException e) {
+            System.out.println(e);
+            System.exit(-3);
+        }
+
+        System.out.println( "Server started" );
+    }
+
+
+    // TODO fare parte di RMI
+
+    @Override
+    public void metodo() throws RemoteException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'metodo'");
+        System.out.println("server risponde");
     }
     
 
-    // TODO fare parte di RMI
+    
 }

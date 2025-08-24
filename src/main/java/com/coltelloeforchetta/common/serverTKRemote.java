956774a2f21cf8553f22ba4b9500a5c44a9f2394
Giaki03 <79930388+Giaki03@users.noreply.github.com>
2025-08-24 14:38:14 +0200

    TODO

--- src/main/java/com/coltelloeforchetta/common/serverTKRemote.java
+++ src/main/java/com/coltelloeforchetta/common/serverTKRemote.java
@@ -6,4 +6,9 @@ import java.rmi.RemoteException;
 public interface serverTKRemote extends Remote{
     void metodo() throws RemoteException;
     int metodo2(int a) throws RemoteException;
+
+    // TODO
+    //ritornare ogni cosa
+    //insert di tutto
+    //modifica recensione
 }

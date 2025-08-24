47fbad734d4436f2b4bc02281debf82f6d34d2bd
BiskoBerty <ricky.galimberti@gmail.com>
2025-08-24 17:00:35 +0200

    prova javaFX

--- src/main/java/com/coltelloeforchetta/client/clientTK.java
+++ src/main/java/com/coltelloeforchetta/client/clientTK.java
@@ -8,8 +8,29 @@
 
 package com.coltelloeforchetta.client;
 
-public class clientTK {
+//mvn clean javafx:run
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class clientTK extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch();
 
     }
 }
+
